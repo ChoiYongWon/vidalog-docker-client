ebe0f8038c396fdd9659bfb08882c4b6431033e9
ChoiYongWon <yongwon0824@naver.com>
2021-07-04 06:33:26 +0900

    :lipstick:Fixed IOS Appearance

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,15 @@ const GlobalStyle = createGlobalStyle`
     height : 100%;
     padding : 0;
     margin : 0;
+    
+  }
+  input, textarea, button {
+    appearance: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    border-radius : 0;
+    -webkit-border-radius : 0;
+    -moz-border-radius : 0;
   }
 `
 
